0f34f8f55b4a1e04207a8f63373dc45e56fe7e1a
Grace Zhang <grace.zhang@admaster.com.cn>
2018-04-14 19:32:45 +0800

    Able to receive urls and response to server with the result

--- src/parallelled-header-request/main.go
+++ src/parallelled-header-request/main.go
@@ -3,6 +3,8 @@ package main
 import (
     "fmt"
     "net/http"
+    "io"
+    "bytes"
     "strings"
     "log"
     "time"
@@ -11,10 +13,7 @@ import (
     "github.com/qiniu/api.v7/auth/qbox"
     "github.com/qiniu/api.v7/storage"
 
-    // "flag"
     "github.com/gin-gonic/gin"
-    // "github.com/spf13/viper"
-    "strconv"
 )
 
 var (
@@ -25,11 +24,13 @@ var (
     bucket = ""
 )
 
-var cvalid = make(chan int);
-var cinvalid = make(chan string);
 var wg sync.WaitGroup
 
-func urlCheck(url string, i int) {
+var client = &http.Client{
+    Timeout: time.Second * 10,
+}
+
+func urlCheck(url string, i int, cvalid chan int, cinvalid chan string) {
     defer wg.Done()
 
     deadline := time.Now().Add(time.Second * 3600 * 2).Unix() //2小时有效期
@@ -65,9 +66,10 @@ func start(w http.ResponseWriter, r *http.Request) {
         fmt.Println("val:", strings.Join(v, ""))
 
         urls := strings.Split(strings.Join(v, ""), ",")
-        for i, v := range urls {
+        // for i, v := range urls {
+        for _, v := range urls {
             fmt.Println("Start checking", v)
-            go urlCheck(v, i)
+            // go urlCheck(v, i)
         }
     }
 
@@ -89,31 +91,63 @@ func start(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Finish checking") // send data to client side
 }
 
+func printSlice(x []string){
+   fmt.Printf("len=%d cap=%d slice=%v\n",len(x),cap(x),x)
+}
+
+func putRequest(url string, data io.Reader)  {
+    client := &http.Client{}
+    req, err := http.NewRequest(http.MethodPut, url, data)
+    if err != nil {
+        // handle error
+        log.Fatal(err)
+    }
+    _, err = client.Do(req)
+    if err != nil {
+        // handle error
+        log.Fatal(err)
+    }
+}
+
 func main() {
     r := gin.Default()
     r.POST("/task", func(c *gin.Context) {
+        var cvalid = make(chan int);
+        var cinvalid = make(chan string);
         id := c.Query("id")
         names := strings.Split(c.PostForm("name_keys"), ",")
-
+        invalidList := make([]string, 4)
         for i, v := range names {
             wg.Add(1)
-            go urlCheck(v+strconv.Itoa(j), i)
+            go urlCheck(v, i, cvalid, cinvalid)
         }
         fmt.Printf("id: %s; urls: %s;", id, names)
-        c.String(http.StatusOK, "success")
+
+        go func() {
+            wg.Wait()
+            close(cvalid)
+            close(cinvalid)
+
+            responseTo := "url_here"
+            data := []byte(`{"urls":"` + strings.Join(invalidList, ",") + `"}`)
+            request, _ := http.NewRequest(http.MethodPut, responseTo, bytes.NewBuffer(data))
+            request.Header.Set("Content-Type", "application/json")
+            _, err := client.Do(request)
+            if err != nil {
+                fmt.Println(err)
+            }
+        }()
 
         for {
             iv, ok := <- cinvalid
             if !ok {
+                fmt.Println("no invalid values")
                 return
             } else {
                 fmt.Println("invalid value:", iv)
+                invalidList = append(invalidList, iv)
             }
         }
-
-        wg.Wait()
-        close(cvalid)
-        close(cinvalid)        
     })
     r.Run()
 }
