f39a85ab71408d114d2807d36aaa3159f1b37d08
Grace Zhang <grace.zhang@admaster.com.cn>
2018-04-28 17:23:56 +0800

    Able to receive array and response to the request

--- src/parallelled-header-request/main.go
+++ src/parallelled-header-request/main.go
@@ -3,10 +3,10 @@ package main
 import (
     "fmt"
     "net/http"
-    "io"
+    // "io"
     "bytes"
     "strings"
-    "log"
+    // "log"
     "time"
     "sync"
     "io/ioutil"
@@ -14,6 +14,8 @@ import (
     "github.com/qiniu/api.v7/storage"
 
     "github.com/gin-gonic/gin"
+    // "encoding/json"
+    // "strconv"
 )
 
 var (
@@ -25,128 +27,78 @@ var (
 )
 
 var wg sync.WaitGroup
+var mutex sync.Mutex
 
 var client = &http.Client{
     Timeout: time.Second * 10,
 }
 
-func urlCheck(url string, i int, cvalid chan int, cinvalid chan string) {
+func urlCheck(url string, i int, cinvalid chan string) {
     defer wg.Done()
 
     deadline := time.Now().Add(time.Second * 3600 * 2).Unix() //2小时有效期
     privateAccessURL := storage.MakePrivateURL(mac, bucket_domain, url, deadline)
-    fmt.Print(".")
+
     resp, err := http.Get(privateAccessURL)
     defer resp.Body.Close()
 
     if err != nil || resp.StatusCode != 200 {
+        fmt.Println("X:",resp.StatusCode)
         cinvalid <- url
     } else {
         _, err1 := ioutil.ReadAll(resp.Body)
         if err1 != nil {
-           cinvalid <- url
+            fmt.Print("X")
+            cinvalid <- url
         } else {
-            cvalid <- i
+            fmt.Print("√")
         }
-    }
-}
-
-func start(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()  // parse arguments, you have to call this by yourself
-    fmt.Println(r.Form)  // print form information in server side
-    fmt.Println("path", r.URL.Path)
-    fmt.Println("scheme", r.URL.Scheme)
-    fmt.Println(r.Form["url_long"])
-    start := time.Now()
-
-    // this wg number should base on the urls count
-    wg.Add(2)
-    for k, v := range r.Form {
-        fmt.Println("key:", k)
-        fmt.Println("val:", strings.Join(v, ""))
-
-        urls := strings.Split(strings.Join(v, ""), ",")
-        // for i, v := range urls {
-        for _, v := range urls {
-            fmt.Println("Start checking", v)
-            // go urlCheck(v, i)
-        }
-    }
-
-    for {
-        iv, ok := <- cinvalid
-        if !ok {
-            return
-        } else {
-            fmt.Println("invalid value:", iv)
-        }
-    }
-
-    wg.Wait()
-    close(cvalid)
-    close(cinvalid)
-
-    elapsed := time.Since(start)
-    log.Printf("Url check took %s", elapsed)
-    fmt.Fprintf(w, "Finish checking") // send data to client side
-}
-
-func printSlice(x []string){
-   fmt.Printf("len=%d cap=%d slice=%v\n",len(x),cap(x),x)
-}
-
-func putRequest(url string, data io.Reader)  {
-    client := &http.Client{}
-    req, err := http.NewRequest(http.MethodPut, url, data)
-    if err != nil {
-        // handle error
-        log.Fatal(err)
-    }
-    _, err = client.Do(req)
-    if err != nil {
-        // handle error
-        log.Fatal(err)
     }
 }
 
 func main() {
     r := gin.Default()
     r.POST("/task", func(c *gin.Context) {
-        var cvalid = make(chan int);
         var cinvalid = make(chan string);
+
         id := c.Query("id")
-        names := strings.Split(c.PostForm("name_keys"), ",")
-        invalidList := make([]string, 4)
-        for i, v := range names {
+        name_keys := strings.Split(c.PostForm("name_keys"), ",")
+        fmt.Printf("id: %s; urls: %s;\n", id, name_keys)
+        length_of_name_keys := len(name_keys)
+
+        var invalidList = make([]string, length_of_name_keys)
+        for i, v := range name_keys {
             wg.Add(1)
-            go urlCheck(v, i, cvalid, cinvalid)
+            go urlCheck(v, i, cinvalid)
         }
-        fmt.Printf("id: %s; urls: %s;", id, names)
-
+        
         go func() {
-            wg.Wait()
-            close(cvalid)
-            close(cinvalid)
-
-            responseTo := "url_here"
-            data := []byte(`{"urls":"` + strings.Join(invalidList, ",") + `"}`)
-            request, _ := http.NewRequest(http.MethodPut, responseTo, bytes.NewBuffer(data))
-            request.Header.Set("Content-Type", "application/json")
-            _, err := client.Do(request)
-            if err != nil {
-                fmt.Println(err)
+            for {
+                iv, ok := <- cinvalid
+                if !ok {
+                    fmt.Println("no invalid values")
+                    close(cinvalid)
+                    wg.Done()
+                    break
+                } else {
+                    fmt.Println("invalid value:", iv)
+                    invalidList = append(invalidList, iv)
+                }
             }
         }()
 
-        for {
-            iv, ok := <- cinvalid
-            if !ok {
-                fmt.Println("no invalid values")
-                return
-            } else {
-                fmt.Println("invalid value:", iv)
-                invalidList = append(invalidList, iv)
-            }
+        wg.Wait()
+        
+        fmt.Println("Finished here!")
+        fmt.Println(invalidList)
+
+        responseTo := ""
+        data := []byte(`{"urls":"` + strings.Join(invalidList, ",") + `"}`)
+        request, _ := http.NewRequest(http.MethodPut, responseTo, bytes.NewBuffer(data))
+        request.Header.Set("Content-Type", "application/json")
+        _, err := client.Do(request)
+        if err != nil {
+            fmt.Println(err)
         }
     })
     r.Run()
